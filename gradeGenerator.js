7af156dd4e57f2ff9ce6cd1502e87bd1c6ab63f5
David Parsley <davidparsley.kakhayanga@gmail.com>
2024-09-29 20:46:58 +0300

    completed assignment

--- gradeGenerator.js
+++ gradeGenerator.js
@@ -1,25 +1,25 @@
 let grade= 79 // INSERT GRADE HERE !
 
 if(grade >=0 && grade <=39){
-    console.log("E");
+    console.log("E"); // If grade is 0 to 39 output will be "E"
 }
 else if( grade >=40 && grade <=48){
-    console.log("D")
+    console.log("D") // If grade is 40 to 48 output will be "D"
 }
 else if(grade >=49 && grade <=59){
     
-    console.log("C")
+    console.log("C") // If grade is 49 to 59 output will be "C"
 }
 else if(grade >=60 && grade <=79){
     
-    console.log("B")
+    console.log("B") // If grade is 60 to 79 output will be "B"
 }
 else if(grade >=80 && grade <=100){
     
-    console.log("A")
+    console.log("A") // If grade is 80 to 100 output will be "A"
 }
 else{
     console.log("Invalid Input")
-}
+}                    // if grade is grater than 100 output will be invalid because a grade above 100 is not possibe
 
 
